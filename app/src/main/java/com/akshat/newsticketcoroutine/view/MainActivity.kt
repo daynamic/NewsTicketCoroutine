480365c3bc10bc807ce6365798179991a7d891d4
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-10-08 12:26:51 -0400

    Initial commit

---
+++ app/src/main/java/com/akshat/newsticketcoroutine/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.akshat.newsticketcoroutine.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akshat.newsticketcoroutine.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
